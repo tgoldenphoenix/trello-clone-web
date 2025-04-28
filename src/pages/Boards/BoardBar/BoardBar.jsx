0eb2536872e9a84be51bda5d6f589069a17200e1
tgoldenphoenix <anhaophamx@gmail.com>
2025-04-28 13:53:41 +0700

    update BoardBard avatars

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -102,34 +102,34 @@ function BoardBar({ board }) {
             }
           }}
         >
-          <Tooltip title="trungquandev">
-            <Avatar alt="trungquandev"
-              src="https://trungquandev.com/wp-content/uploads/2023/05/main-avatar-circle-min-trungquandev-codetq.jpeg"
+          <Tooltip title="Gandalf">
+            <Avatar alt="Gandalf"
+              src="https://static1.srcdn.com/wordpress/wp-content/uploads/2017/07/Sir-Ian-McKellen-as-Gandalf-The-Grey-The-Shire-Lord-of-the-Rings-Peter-Jackson.jpg"
             />
           </Tooltip>
-          <Tooltip title="trungquandev">
-            <Avatar alt="trungquandev"
-              src="https://trungquandev.com/wp-content/uploads/2021/01/trungquandev-avatar-2021.jpg"
+          <Tooltip title="Arya">
+            <Avatar alt="Arya"
+              src="https://pyxis.nymag.com/v1/imgs/846/9bb/440e83edacba3579e42bb6ad20860b50b0-18-arya-stark.rsquare.w400.jpg"
             />
           </Tooltip>
-          <Tooltip title="trungquandev">
-            <Avatar alt="trungquandev"
-              src="https://trungquandev.com/wp-content/uploads/2018/04/trungquandev-avatar.jpeg"
+          <Tooltip title="Bilbo">
+            <Avatar alt="Bilbo"
+              src="https://cdn.costumewall.com/wp-content/uploads/2018/10/bilbo-baggins.jpg"
             />
           </Tooltip>
-          <Tooltip title="trungquandev">
-            <Avatar alt="trungquandev"
-              src="https://trungquandev.com/wp-content/uploads/2019/03/trungquandev-avatar-01-scaled.jpg"
+          <Tooltip title="Goku">
+            <Avatar alt="Goku"
+              src="https://f8n-production.s3.amazonaws.com/creators/profile/qhhd8h2kd-songoku-png-az0y8z.png"
             />
           </Tooltip>
-          <Tooltip title="trungquandev">
-            <Avatar alt="trungquandev"
-              src="https://trungquandev.com/wp-content/uploads/2017/03/aboutme.jpg"
+          <Tooltip title="Cổ Thiên Lạc">
+            <Avatar alt="Cổ Thiên Lạc"
+              src="https://image.phunuonline.com.vn/fckeditor/upload/2022/20220805/images/10-nam-qua-co-thien-lac-chi-_831659675187.jpg"
             />
           </Tooltip>
-          <Tooltip title="trungquandev">
-            <Avatar alt="trungquandev"
-              src="https://trungquandev.com/wp-content/uploads/2019/06/trungquandev-cat-avatar.png"
+          <Tooltip title="Trịnh Thiếu Thu">
+            <Avatar alt="Trịnh Thiếu Thu"
+              src="https://iv1cdn.vnecdn.net/giaitri/images/web/2023/10/04/trinh-thieu-thu-tren-man-anh-1696392663.jpg?w=900&h=540&q=100&dpr=1&fit=crop&s=88JlqWLX3FfmwQfjfQhEKw"
             />
           </Tooltip>
 
